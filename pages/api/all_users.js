5f7a0e36fa181aad5dbc0039a93ff16c04dd9aaf
Arcee <lucenoarcee0@gmail.com>
2024-11-04 02:16:38 +0800

    chore: Update all_users API to handle errors during user fetching and added vercel.json.

--- pages/api/all_users.js
+++ pages/api/all_users.js
@@ -1,15 +1,9 @@
 import connectDatabase from "../../lib/connectDatabase";
 import User from "../../model/User";
-
 export default async function (req, res) {
-  try {
-    await connectDatabase();
+  await connectDatabase();
 
-    const users = await User.find();
+  const users = await User.find();
 
-    res.status(200).json({ users });
-  } catch (error) {
-    console.error("An error occurred while fetching users:", error);
-    res.status(500).json({ error: "An error occurred while fetching users" });
-  }
+  res.json({ users });
 }
