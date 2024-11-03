b5bcaaf096587c942583d651fd3b1ccc7bffaab9
Arcee <lucenoarcee0@gmail.com>
2024-11-04 02:04:08 +0800

    chore: Update all_users API to handle errors during user fetching

--- pages/api/all_users.js
+++ pages/api/all_users.js
@@ -1,9 +1,15 @@
 import connectDatabase from "../../lib/connectDatabase";
 import User from "../../model/User";
+
 export default async function (req, res) {
-  await connectDatabase();
+  try {
+    await connectDatabase();
 
-  const users = await User.find();
+    const users = await User.find();
 
-  res.json({ users });
+    res.status(200).json({ users });
+  } catch (error) {
+    console.error("An error occurred while fetching users:", error);
+    res.status(500).json({ error: "An error occurred while fetching users" });
+  }
 }
